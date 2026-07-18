4feb5e5520e8abdb1af2a66f10cd35cd2b8c77b1
Anshul Jat <anshuljat90@gmail.com>
2026-07-19 01:22:53 +0530

    code

--- python.py
+++ python.py
@@ -1,2 +1,31 @@
-print("Hello Anshul")
+print("Hello, Anshul!")
 
+# print all data type 
+"""
+a = [10 , 20 , 'anshul']
+print(type(a)) 
+b = (10 , 20 , 'anshul')
+print(type(b))
+c = {10 , 20 , 'anshul'}
+print(type(c))
+d = frozenset({10 , 20 , 'anshul'})
+print(type(d))
+e = {'name' : 'anshul', 'age' : '18'}
+print(type(e))
+
+"""
+
+# print all arithmatic operator 
+
+"""
+a = 10 
+b = 5 
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+
+"""
