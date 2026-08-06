15b355b03978310754e82e6f6c0f45b1285a15cb
Anshul Jat <anshuljat90@gmail.com>
2026-08-06 19:07:09 +0530

    code

--- python_code_practice.py
+++ python_code_practice.py
@@ -1084,3 +1084,22 @@ with open("practice.txt", "r") as f:
         print("Not Found")
 
 """
+
+# WAF to find in which line of the file does the word "learning" occur first. print -1 if word is not found in the file.
+
+"""
+def find_word_in_file():
+    word = "learning"
+    data = True 
+    line_no = 1 
+    with open("practice.txt", "r") as f:
+        while data:
+            data = f.readline()
+            if(word in data):
+                print(line_no)
+                return 
+            line_no += 1
+    return -1
+
+find_word_in_file()
+"""
